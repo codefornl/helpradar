dfcc713b4facade50e49fdbce42c31426aa07bbe
milovanderlinden <milo@dogodigi.net>
2020-04-07 18:31:59 +0200

    Nog een keer geocoden en update conflicten

--- platformen/database.py
+++ platformen/database.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, ForeignKey, Integer, String, Text, Float
-=======
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
->>>>>>> 7d80ae4... Now merging various sites into a sqlite db
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -22,13 +18,10 @@ class Initiative(Base):
     source_id = Column(String(250))
     frequency = Column(String(250))
     location = Column(String(250))
-<<<<<<< HEAD
     latitude = Column(Float)
     longitude = Column(Float)
     osm_address = Column(Text)
 
-=======
->>>>>>> 7d80ae4... Now merging various sites into a sqlite db
 
 
 class Db:
@@ -37,11 +30,7 @@ class Db:
     def __init__(self):
         # Create an engine that stores data in the local directory's
         # sqlalchemy_example.db file.
-<<<<<<< HEAD
         engine = create_engine('sqlite:///helpradar.db')
-=======
-        engine = create_engine('sqlite:///corona-data.db')
->>>>>>> 7d80ae4... Now merging various sites into a sqlite db
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         self.session = Session()
