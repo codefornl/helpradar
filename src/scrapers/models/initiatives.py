8e848116d10e9fffbd9d9ce845d7787903141726
Ferry de Boer <ferry.de.boer@gmail.com>
2020-04-25 17:01:50 +0200

    Added batch import table and new basic initiative structure.

--- src/scrapers/models/initiatives.py
+++ src/scrapers/models/initiatives.py
@@ -5,38 +5,37 @@ from sqlalchemy.orm import relationship, sessionmaker
 Base = declarative_base()
 
 # Base class for both platforms and helpinitiatives
-#class InitiativeBase(Base):
-#    __tablename__ = 'initiatives'
-#    id = Column(Integer, primary_key=True)
-#    name = Column(String(200))
-#    description = Column(Text)
-#    url = Column(String(500))
-#    latitude = Column(Float)
-#    longitude = Column(Float)
-
-#    discriminator = Column('type', String(50))
-#    __mapper_args__ = {'polymorphic_on': discriminator}
+class InitiativeBase(Base):
+    __tablename__ = 'initiatives'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    description = Column(Text)
+    url = Column(String(500))
+    latitude = Column(Float)
+    longitude = Column(Float)
+    discriminator = Column('type', String(50))
+    __mapper_args__ = {'polymorphic_on': discriminator}
 
 # so far only a basic definition of platforms.
-#class Platform(InitiativeBase):
-#    __tablename__ = 'platforms'
-#    __mapper_args__ = {'polymorphic_identity': 'platform'}
-#    id = Column(Integer, ForeignKey('initiatives.id'), primary_key=True)
-#    place = Column(Text)
+class Platform(InitiativeBase):
+    __tablename__ = 'platforms'
+    __mapper_args__ = {'polymorphic_identity': 'platform'}
+    id = Column(Integer, ForeignKey('initiatives.id'), primary_key=True)
+    place = Column(Text)
 
 # Group each import run in a batch for later importing.
-#class ImportBatch(Base):
-#    __tablename__ = 'importbatches'
-#    id = Column(Integer, primary_key=True)
-#    platform_id = Column(Integer, ForeignKey('platforms.id'), nullable=False)
-#    started_at = Column(DateTime, nullable=False)
-#    stopped_at = Column(DateTime, nullable=False)
-#    state = Column(Enum("running", "imported", "failed", "processed", "processing_error"), nullable=False)
+class ImportBatch(Base):
+    __tablename__ = 'importbatches'
+    id = Column(Integer, primary_key=True)
+    platform_id = Column(Integer, ForeignKey('platforms.id'), nullable=False)
+    started_at = Column(DateTime, nullable=False)
+    stopped_at = Column(DateTime, nullable=False)
+    state = Column(Enum("running", "imported", "failed", "processed", "processing_error"), nullable=False)
 
 class InitiativeImport(Base):
     __tablename__ = 'initiative_imports'
     id = Column(Integer, primary_key=True)
-    #batch_id = Column(Integer, ForeignKey('importbatches.id'), nullable=False)
+    batch_id = Column(Integer, ForeignKey('importbatches.id'))
     category = Column(String(250))
     group = Column(String(250))
     description = Column(Text)
