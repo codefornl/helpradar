d5e52d93498a7f3b0371a97a2b67b091d32050ea
Ferry de Boer <ferry.de.boer@gmail.com>
2020-04-26 14:18:25 +0200

    Added storing within a batch.

--- src/scrapers/models/__init__.py
+++ src/scrapers/models/__init__.py
@@ -1,3 +1,2 @@
 from .database import Db
-#from .initiatives import InitiativeBase, Platform, ImportBatch, InitiativeImport
-from .initiatives import InitiativeImport
+from .initiatives import InitiativeBase, Platform, ImportBatch, InitiativeImport, BatchImportState, InitiativeGroup
--- src/scrapers/models/database.py
+++ src/scrapers/models/database.py
@@ -1,9 +1,8 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Text, Float
-from sqlalchemy.ext.declarative import declarative_base
+import os
+from pathlib import PurePath
+
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
-
-
 
 class Db:
     session = None
@@ -11,10 +10,11 @@ class Db:
     def __init__(self):
         # Create an engine that stores data in the local directory's
         # sqlalchemy_example.db file.
-        engine = create_engine('sqlite:///helpradar.db')
+        
+        path = PurePath(os.path.dirname(__file__))
+        print(path.parent)
+        engine = create_engine('sqlite:///' + str(path.parent) + '\\helpradar.db')
         #Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
         self.session = Session()
-        # Create all tables in the engine. This is equivalent to "Create Table"
-        # statements in raw SQL.
         
