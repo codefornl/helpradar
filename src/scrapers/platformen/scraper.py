1dd3db5cbac711a248f9e946ac0d1c111a37bdea
Ferry de Boer <ferry.de.boer@gmail.com>
2020-05-12 16:19:42 +0200

    Disabled geocoder from overwriting existing lat/long

--- src/scrapers/platformen/scraper.py
+++ src/scrapers/platformen/scraper.py
@@ -153,6 +153,7 @@ class Scraper(ABC):
             initiative.scraped_at = datetime.utcnow()
             initiative.source = self.platform_url
             self.add_initiative(initiative)
+            self.get_logger().debug(f"Scraped {initiative.source_uri}")
         except ScrapeException as e:
             self.get_logger()\
                 .exception(f"Error while collecting initiative {initiative.source_uri}")
